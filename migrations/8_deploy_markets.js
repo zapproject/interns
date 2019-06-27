87eb5975e4d4952f5611b6d1c8813b1047f4e4c2
Alex <alpuga8@gmail.com>
2019-06-27 15:35:31 -0400

    Fixed Migration Bugs

--- migrations/8_deploy_markets.js
+++ migrations/8_deploy_markets.js
@@ -1,17 +1,16 @@
 var MainMarket = artifacts.require('./MainMarket.sol');
 var AuxiliaryMarket = artifacts.require('./AuxiliaryMarket.sol');
 var ZapCoordinator = artifacts.require('./ZapCoordinator.sol');
-
 const MainMarketToken = artifacts.require('./MainMarketToken.sol');
 
 module.exports = async function(deployer) {
   const coordInstance = await ZapCoordinator.deployed();
+  await deployer.deploy(MainMarketToken);
+  await deployer.deploy(AuxiliaryMarket, ZapCoordinator.address);
+  await deployer.deploy(MainMarket, ZapCoordinator.address);
   await coordInstance.addImmutableContract('MAINMARKET', MainMarket.address);
   await coordInstance.addImmutableContract(
     'AUXMARKET',
     AuxiliaryMarket.address
   );
-  await deployer.deploy(MainMarketToken, 'Market', 'MMT');
-  await deployer.deploy(AuxiliaryMarket);
-  await deployer.deploy(MainMarket);
 };
