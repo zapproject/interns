4100e4855f69a4500ae7a18b0d8d8d0c5ecf16e1
tonytran114 <tonytran@umass.edu>
2019-06-28 18:07:15 -0400

    changed deploy_market migration

--- migrations/8_deploy_markets.js
+++ migrations/8_deploy_markets.js
@@ -2,11 +2,12 @@ var MainMarket = artifacts.require('./MainMarket.sol');
 var AuxiliaryMarket = artifacts.require('./AuxiliaryMarket.sol');
 var ZapCoordinator = artifacts.require('./ZapCoordinator.sol');
 const MainMarketToken = artifacts.require('./MainMarketToken.sol');
+var coordInstance;
 
 module.exports = async function(deployer) {
-	const coordInstance = await ZapCoordinator.deployed();
+	coordInstance = await ZapCoordinator.deployed();
 	await deployer.deploy(MainMarketToken);
-	await deployer.deploy(AuxiliaryMarket, ZapCoordinator.address);
+	await deployer.deploy(AuxiliaryMarket);
 	await deployer.deploy(MainMarket, ZapCoordinator.address);
 	await coordInstance.addImmutableContract('MAINMARKET', MainMarket.address);
 	await coordInstance.addImmutableContract('AUXMARKET', AuxiliaryMarket.address);
