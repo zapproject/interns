268c9a31afb171fa4bb790f956240b747d8d7c0b
mezaisrael <meza.israel.91@gmail.com>
2019-07-06 17:05:01 -0400

    edit migration file

--- migrations/8_deploy_markets.js
+++ migrations/8_deploy_markets.js
@@ -17,12 +17,17 @@ module.exports = async function(deployer) {
   await coordinator.addImmutableContract('MAINMARKET', mm.address);
   await coordinator.addImmutableContract('AUXMARKET', aux.address);
 
+  var zapInWei = await mm.zapInWei();
+
   //Mint initial 100 million MMT Tokens for Main Market to disperse to users who bond
-  await mmt.mint(mm.address, 100000000);
+  await mmt.mint(mm.address, "10000000000000000" );
+
+  var allocate = 500* zapInWei.toNumber();
 
   //Allocate 500 Zap to user for testing purposes locally
-  await mm.allocateZap(500);
+  await mm.allocateZap(allocate + "");
 
+  var approved = 100* zapInWei.toNumber();
   //Approve MainMarket an allowance of 100 Zap to use on behalf of msg.sender(User)
-  await zapToken.approve(mm.address, 100)
+  await zapToken.approve(mm.address, approved);
 };
