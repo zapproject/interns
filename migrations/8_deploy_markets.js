7b71057547e1aa3249c4f6b36e5a5034bdb1fc02
Alex <alpuga8@gmail.com>
2019-07-11 16:20:15 -0400

    Sell Function Implementation

--- migrations/8_deploy_markets.js
+++ migrations/8_deploy_markets.js
@@ -6,61 +6,59 @@ const AuxiliaryMarketToken = artifacts.require('./AuxiliaryMarketToken.sol');
 const ZapToken = artifacts.require('./ZapToken.sol');
 
 module.exports = async function(deployer) {
-	const coordinator = await ZapCoordinator.deployed();
-	const zapToken = await ZapToken.deployed();
-	await deployer.deploy(MainMarketToken);
-	await deployer.deploy(AuxiliaryMarketToken);
-	const mmt = await MainMarketToken.deployed();
-	const amt = await AuxiliaryMarketToken.deployed();
-	await coordinator.addImmutableContract('MAINMARKET_TOKEN', mmt.address);
-	await coordinator.addImmutableContract('AUXILIARYMARKET_TOKEN', amt.address);
-	await deployer.deploy(AuxiliaryMarket, ZapCoordinator.address);
-	await deployer.deploy(MainMarket, ZapCoordinator.address);
-	const mm = await MainMarket.deployed();
-	const am = await AuxiliaryMarket.deployed();
-	await coordinator.addImmutableContract('MAINMARKET', mm.address);
-	await coordinator.addImmutableContract('AUXMARKET', am.address);
+  const coordinator = await ZapCoordinator.deployed();
+  const zapToken = await ZapToken.deployed();
+  await deployer.deploy(MainMarketToken);
+  await deployer.deploy(AuxiliaryMarketToken);
+  const mmt = await MainMarketToken.deployed();
+  const amt = await AuxiliaryMarketToken.deployed();
+  await coordinator.addImmutableContract('MAINMARKET_TOKEN', mmt.address);
+  await coordinator.addImmutableContract('AUXILIARYMARKET_TOKEN', amt.address);
+  await deployer.deploy(MainMarket, ZapCoordinator.address);
+  const mm = await MainMarket.deployed();
+  await coordinator.addImmutableContract('MAINMARKET', mm.address);
+  await deployer.deploy(AuxiliaryMarket, ZapCoordinator.address);
+  const am = await AuxiliaryMarket.deployed();
+  await coordinator.addImmutableContract('AUXMARKET', am.address);
 
-	var accounts = web3.eth.getAccounts();
-	var secondAccount = accounts[1];
-	var thirdAccount = accounts[2];
+  var accounts = web3.eth.getAccounts();
+  var secondAccount = accounts[1];
+  var thirdAccount = accounts[2];
 
-	//Mint initial 100 million MMT Tokens for Main Market to disperse to users who bond
-	var mintAmount = 100000000;
+  //Mint initial 100 million MMT Tokens for Main Market to disperse to users who bond
+  var mintAmount = 100000000;
 
-	//turn to 18 decimal precision
-	let mmtWei = web3.utils.toWei(mintAmount.toString(), 'ether');
-	let amtWei = web3.utils.toWei(mintAmount.toString(), 'ether');
-	//mmtWei is used for more precise transactions.
-	await mmt.mint(mm.address, mmtWei);
-	await amt.mint(am.address, amtWei);
+  //turn to 18 decimal precision
+  let mmtWei = web3.utils.toWei(mintAmount.toString(), 'ether');
+  let amtWei = web3.utils.toWei(mintAmount.toString(), 'ether');
+  //mmtWei is used for more precise transactions.
+  await mmt.mint(mm.address, mmtWei);
+  await amt.mint(am.address, amtWei);
 
-	let allocate = 2000000;
-	let allocateInWeiMMT = web3.utils.toWei(allocate.toString(), 'ether');
+  let allocate = 2000000;
+  let allocateInWeiMMT = web3.utils.toWei(allocate.toString(), 'ether');
 
-	//Allocate 500 Zap to user for testing purposes locally
-	await mm.allocateZap(allocateInWeiMMT);
+  //Allocate 500 Zap to user for testing purposes locally
+  await mm.allocateZap(allocateInWeiMMT);
 
-	//100 zap
-	let approved = 2000000;
-	let approveWeiZap = web3.utils.toWei(approved.toString(), 'ether');
+  //100 zap
+  let approved = 2000000;
+  let approveWeiZap = web3.utils.toWei(approved.toString(), 'ether');
 
-	//Approve MainMarket an allowance of 100 Zap to use on behalf of msg.sender(User)
-	await zapToken.approve(mm.address, approveWeiZap);
-	await zapToken.approve(am.address, approveWeiZap)
+  //Approve MainMarket an allowance of 100 Zap to use on behalf of msg.sender(User)
+  await zapToken.approve(mm.address, approveWeiZap);
+  await zapToken.approve(am.address, approveWeiZap);
 
-
-	//Setting up second account with zap and approving zap to withdraw
-	// await mm.allocateZap(allocateInWeiMMT, {from: secondAccount});
-	// await zapToken.approve(mm.address, approveWeiZap, {from: secondAccount});
-	// await mm.depositZap(approveWeiZap, {from: secondAccount});
-	// await mm.bond(10, {from: secondAccount});
-	//
-	//
-	// //Setting up third account with zap and approving zap to withdraw
-	// await mm.allocateZap(allocateInWeiMMT, {from: thirdAccount});
-	// await zapToken.approve(mm.address, approveWeiZap, {from: thirdAccount});
-	// await mm.depositZap(approveWeiZap, {from: thirdAccount});
-	// await mm.bond(50, {from: thirdAccount});
-
+  //Setting up second account with zap and approving zap to withdraw
+  // await mm.allocateZap(allocateInWeiMMT, {from: secondAccount});
+  // await zapToken.approve(mm.address, approveWeiZap, {from: secondAccount});
+  // await mm.depositZap(approveWeiZap, {from: secondAccount});
+  // await mm.bond(10, {from: secondAccount});
+  //
+  //
+  // //Setting up third account with zap and approving zap to withdraw
+  // await mm.allocateZap(allocateInWeiMMT, {from: thirdAccount});
+  // await zapToken.approve(mm.address, approveWeiZap, {from: thirdAccount});
+  // await mm.depositZap(approveWeiZap, {from: thirdAccount});
+  // await mm.bond(50, {from: thirdAccount});
 };
