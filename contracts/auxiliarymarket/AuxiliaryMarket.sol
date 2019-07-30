b8227646bc316f9e08c7e1931b022e9db5c654d3
Moin Uddin <U_Moin75@yahoo.com>
2019-07-29 21:45:48 -0400

    Implement functionality with an OffChain Oracle that queries based on symbol

--- contracts/auxiliarymarket/AuxiliaryMarket.sol
+++ contracts/auxiliarymarket/AuxiliaryMarket.sol
@@ -16,10 +16,10 @@ contract AuxiliaryMarket is AuxiliaryMarketInterface {
     struct AuxMarketHolder{
         uint256 avgPrice;
         uint256 tokens;
-        Order order;
     }
 
     struct Order{
+        address sender;
         uint256 _quantity;
         Action action;
     }
@@ -27,18 +27,18 @@ contract AuxiliaryMarket is AuxiliaryMarketInterface {
     enum Action { BUY, SELL }
 
     mapping (address => AuxMarketHolder) holders;
-    mapping (uint256 => address) queries;
+    mapping (uint256 => Order) queries;
 
     ZapCoordinatorInterface public coordinator;
     ZapToken public zapToken;
     MainMarketInterface public mainMarket;
     AuxiliaryMarketTokenInterface public auxiliaryMarketToken;
     DispatchInterface public dispatch;
-
+    BondageInterface public bondage;
 
     uint precision = 10 ** 18;
     uint weiZap = precision;
-    bytes32 symbol;
+    bytes32 assetSymbol;
     string assetClass;
 
 
@@ -48,61 +48,67 @@ contract AuxiliaryMarket is AuxiliaryMarketInterface {
         mainMarket = MainMarketInterface(coordinator.getContract("MAINMARKET"));
         auxiliaryMarketToken = AuxiliaryMarketTokenInterface(coordinator.getContract("AUXILIARYMARKET_TOKEN"));
         zapToken = ZapToken(coordinator.getContract("ZAP_TOKEN"));
-        //Assign the Symbol and AssetClass 
-        symbol = 0x4254430000000000000000000000000000000000000000000000000000000000;
+        bondage = BondageInterface(coordinator.getContract("BONDAGE"));
+        assetSymbol = 0x4254430000000000000000000000000000000000000000000000000000000000; //BTC
         assetClass = "cryptocurrency";
     }
 
     function buy(uint256 _quantity) public{
-        uint256 id = executeTransaction(_quantity, "BUY");
-        queries[id] = msg.sender;
+        executeTransaction(_quantity, Action.BUY);
     }
 
     function sell(uint256 _quantity) public hasApprovedAMT(_quantity) hasEnoughAMT(_quantity) {
-        uint256 id = executeTransaction(_quantity, "SELL");
-        queries[id] = msg.sender;
+        executeTransaction(_quantity, Action.SELL);
     }
 
-    function executeTransaction(uint256 _quantity, string memory action) private returns (uint256) {
-        bytes32 bytes32Quantity = bytes32(_quantity);
-        //bytes32 zapSymbol = bytes32("ZAP");
-        //bytes32 bytes32Action = bytes32(action);
-        //bytes32[] memory params = new bytes32[](3);
-        //params = [bytes32Quantity, symbol];
+    function executeTransaction(uint256 _quantity, Action action) private returns (uint256) {
+        address bondageAddress = coordinator.getContract("BONDAGE");
+        uint256 auxiliaryContractZapBalance = zapToken.balanceOf(address(this));
+        zapToken.approve(bondageAddress, auxiliaryContractZapBalance);
+        bytes32[] memory bytes32Arr = new bytes32[](2);
+        bytes32 zapSymbol = 0x5a41500000000000000000000000000000000000000000000000000000000000;
+        bytes32Arr[0] = zapSymbol;
+        bytes32Arr[1] = assetSymbol;
         address oracleAddress = 0x6cb027Db7C5aAd7c181092c80Bdb4a18043a2EBa;
-        uint256 id = dispatch.query(oracleAddress, assetClass, 0x4173736574204d61726b65740000000000000000000000000000000000000000, [symbol]);
+        bytes32 assetMarketEndpoint = 0x4173736574204d61726b65740000000000000000000000000000000000000000;
+        bondage.bond(oracleAddress, assetMarketEndpoint, 1);
+        uint256 id = dispatch.query(oracleAddress, assetClass, assetMarketEndpoint, bytes32Arr);
+        Order memory order = Order(msg.sender, _quantity, action);
+        queries[id] = order;
         return id;
     }
 
-    function callback(uint256 id, string calldata response1, string calldata response2, string calldata response3, string calldata response4) external {
-        emit Results(response1, response2, response3, response4);
-        address userAddress = queries[id];
+    function callback(uint256 id, string calldata response1, string calldata response2) external {
+        emit Results(response1, response2, "NOTAVAILABLE", "NOTAVAILABLE");
+        Order storage order = queries[id];
+        address sender = order.sender;
+        uint256 _quantity = order._quantity;
+        Action action = order.action;
         uint256 zapInWei = stringToUint(response1);
-        uint256 currentAssetPrice = stringToUint(response2);
+        uint256 currentAssetPrice = 2;
         uint256 weiInWeiZap = weiZap.div(zapInWei);
-        uint256 _quantity = stringToUint(response3);
         uint256 totalWeiZap = weiToWeiZap(currentAssetPrice, weiInWeiZap, _quantity);
-        if(keccak256(bytes(response4)) == keccak256(bytes("BUY"))) {
-            require(getZapBalance(userAddress) > totalWeiZap, "Not enough Zap in Wallet");
-            exchange(userAddress, totalWeiZap, _quantity);
+        if(action == Action.BUY) {
+            require(getZapBalance(sender) > totalWeiZap, "Not enough Zap in Wallet");
+            exchange(sender, totalWeiZap, _quantity);
             calculateAveragePrice(currentAssetPrice, _quantity);
         } 
-        else if(keccak256(bytes(response4)) == keccak256(bytes("SELL"))) {
+        else if(action == Action.SELL) {
             require(getZapBalance(address(mainMarket)) > totalWeiZap, "Not enough Zap in MainMarket");
-            mainMarket.withdraw(totalWeiZap, userAddress);
-            auxiliaryMarketToken.transferFrom(userAddress, address(this), _quantity);
+            mainMarket.withdraw(totalWeiZap, sender);
+            auxiliaryMarketToken.transferFrom(sender, address(this), _quantity);
         } 
         else {
             revert("Invalid Action");
         }
     }
 
-    function stringToUint(string memory s) public returns (uint) {
+    function stringToUint(string memory s) private returns (uint) {
         bytes memory b = bytes(s);
         uint result = 0;
         for (uint i = 0; i < b.length; i++) { // c = b[i] was not needed
-            if (b[i] >= 48 && b[i] <= 57) {
-                result = result * 10 + (uint(b[i]) - 48); // bytes and int are not compatible with the operator -.
+            if (uint(uint8(b[i])) >= 48 && uint(uint8(b[i])) <= 57) {
+                result = result * 10 + (uint(uint8(b[i])) - 48); // bytes and int are not compatible with the operator -.
             }
         }
         return result; 
